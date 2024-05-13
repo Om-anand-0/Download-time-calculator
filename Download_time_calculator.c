f98cfeed4c36953700805e3960d21d1317519fcb
Om-anand-0 <om.d3v.21@gmail.com>
2024-05-13 20:47:18 +0530

    Made it GB and made some changes in logic

--- Download_time_calculator.c
+++ Download_time_calculator.c
@@ -3,11 +3,12 @@
 int main()
 {
     float down_speed, Total_size,time;
-    printf("Enter the Total size of file : \n");
+    printf("Enter the Total size of file in GB : \n");
     scanf("%f", &Total_size);
-    printf("Enter download speedl: \n");
+    printf("Enter download speed in Mb/s: \n");
     scanf("%f", &down_speed);
-    time = Total_size/down_speed;
-    printf("Total time required to download the file is %f seconds", time);
+    time = Total_size*1024/down_speed;
+    time = time/60;
+    printf("Total time required to download the file is %.2f minutes & minutes \n", time);
     return 0;
 }
