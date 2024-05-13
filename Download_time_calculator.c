7e691042539f1a43301b576c0d0823e348684e3b
Om-anand-0 <om.d3v.21@gmail.com>
2024-05-13 20:55:30 +0530

    Added downloaded size and also some changes in logoic

--- Download_time_calculator.c
+++ Download_time_calculator.c
@@ -2,13 +2,16 @@
 
 int main()
 {
-    float down_speed, Total_size,time;
+    float down_speed, Total_size,time, downloaded_size;
     printf("Enter the Total size of file in GB : \n");
     scanf("%f", &Total_size);
+    printf("Enter the Downloaded size of file in GB : \n");
+    scanf("%f", &downloaded_size);
     printf("Enter download speed in Mb/s: \n");
     scanf("%f", &down_speed);
-    time = Total_size*1024/down_speed;
+    downloaded_size = Total_size*1024-downloaded_size*1024;
+    time = downloaded_size/down_speed;
     time = time/60;
-    printf("Total time required to download the file is %.2f minutes & minutes \n", time);
+    printf("Total time required to download the file is %.2f minutes & seconds \n", time);
     return 0;
 }
